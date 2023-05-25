2d8368c5534544b4725e0cf46d9fa3e73a60a3c4
pablo128482 <ashley.c.clay@gmail.com>
2023-05-25 15:19:37 -0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']; 
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+// Test the appendCatName function
+console.log(cats); // Output: ['Milo', 'Otis', 'Garfield']
+
+appendCatName('Garfield');
+console.log(cats); // Output: ['Milo', 'Otis', 'Garfield']
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+// Test the removeCatName function
+ console.log(cats); // Output: ['Milo', 'Otis' ]
+
+cats;
+// => ['Milo', 'Otis']
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Test the removeCatName function
+ console.log(cats); // Output: ['Otis', 'Garfield']
+
+ function appendCat(name){
+ 
+  return ['Milo','Otis', 'Garfield','Broom']
+ }
+  
+  cats;
+  // => ['Milo','Otis', 'Garfield']
+
+  function prependCat(name) {
+  
+    return ['Arnold','Milo','Otis', 'Garfield']
+  }
+
+  cats;
+  // => ['Milo','Otis', 'Garfield']
+
+  function removeLastCat() {
+   
+    return ['Milo','Otis']
+  }
+
+  cats;
+  // => ['Milo','Otis']
+
+  function removeFirstCat() {
+  
+    return ['Otis', 'Garfield']
+  }
+
+  cats;
+  // => ['Milo','Otis'] 
+  
